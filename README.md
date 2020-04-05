1c6eec08c38a4a51ed554438ed2cf028cfdc59b8
Todd Cunningham <tcunningham07@gmail.com>
2020-04-05 14:26:52 +1000

    Integrate gmusicapi to query google play and create playlist
    List of strings from spotify formatted "{artist_name} - {song_title}"
    now generated and used to query on google play using gmusicapi. Also
    optional inclusion to create new playlist. Will print information about
    the failed queries on google play (song/artist not found)

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Spotipy-gmusicapi-converter
 A python script used to import a playlist from Spotify using spotipy and create a Google Play Music playlist using gmusicapi
+
+# Example Usage
+```
+python3 convert.py
+Obtained list of 411 tracks from spotify playlist!
+"Sarcastic Sounds - I Don't Sleep" - no result
+"Sarcastic Sounds - Make Me Smile" - no result
+"Beowülf - Today Is a Gift" - no result
+"Beowülf - Please Don't Go" - no result
+"Sarcastic Sounds - Suddenly You're in Love" - no result
+"Nohidea - falling down (feat. shiloh)" - no result
+"Fudasca - I Wrote You a Letter" - no result
+...
+(Potentially some errors from gmusicapi for songs missing certain info, seems to be a gmusicapi bug - may investigate in the future)
+2020-04-05 14:21:15,741 - gmusicapi.Mobileclient1 (shared:279) [ERROR]: the response format for Search was not recognized.
+...
+"o k h o - Long Espresso" - no result
+"Stan Forebee - Perfect Your Own Art" - no result
+"Beowülf - Hopeless" - no result
+"importmedia - Happy Moments" - no result
+"Orca Vibes - Intuition" - no result
+"Late June - Running In The Rain" - no result
+"Late June - Balcony Pt 2" - no result
+Finished searching 411 tracks on Google Play Music
+Found: 376, Missing: 35
+```
+
+If you've uncommented the last three lines, you should have successfully created a playlist on Google Play Music!
--- convert.py
+++ convert.py
@@ -1,22 +1,31 @@
+# Make sure these exist in your environment for spotipy!
+# export SPOTIPY_CLIENT_ID="YOUR_SPOTIFY_CIENT_ID"
+# export SPOTIPY_CLIENT_SECRET="YOUR_SPOTIFY_CLIENT_SECRET"
+
 from gmusicapi import Mobileclient
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 spotify_tracks = []
 
+# Spotify playlist ID that you would like to convert
+# Currently, this playlist must be public.
+# I've obtained this via URL from browser web-app
+# (i.e. https://open.spotify.com/playlist/7jNaXgyXgipE4RNcNCBYmW)
+spotify_playlist_id = '7jNaXgyXgipE4RNcNCBYmW'
+
 # Print
 def show_tracks(tracks):
     for i, item in enumerate(tracks['items']):
         track = item['track']
         # print(f" {i} {track['artists'][0]['name']} {track['name']}")
-        track_info = f" {track['artists'][0]['name']} {track['name']}"
+        track_info = f"{track['artists'][0]['name']} - {track['name']}"
         spotify_tracks.append(track_info)
     return len(tracks['items'])
 
-
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-results = sp.playlist('7jNaXgyXgipE4RNcNCBYmW',  fields="tracks,next")
+results = sp.playlist(spotify_playlist_id,  fields="tracks,next")
 tracks = results['tracks']
 show_tracks(tracks)
 
@@ -26,5 +35,38 @@ while tracks['next']:
     # spotify_tracks.append(tracks)
     num_tracks += show_tracks(tracks)
 
-print(f"Total tracks: {num_tracks}")
-print(f'Count: {len(spotify_tracks)}')
+print(f"Obtained list of {len(spotify_tracks)} tracks from spotify playlist!")
+
+api = Mobileclient()
+
+# Run perform_oath() the first time!
+# api.perform_oauth()
+
+api.oauth_login('YOUR_GOOGLE_PLAY_AUTH_TOKEN')
+
+missing_data = 0
+google_play_ids = []
+
+for track in spotify_tracks:
+    result = {}
+    try:
+        result = api.search(track, max_results=1)
+    except:
+        # Currently there are some gmusicapi exceptions here that show up on stdout
+        # but the app will continue searching each track and eventually complete
+        print(f'Error searching "{track}"')
+
+    if(not result['song_hits'] or not len(result['song_hits'])):
+        missing_data += 1
+        print(f'"{track}" - no result')
+    else:
+        google_play_ids.append(result['song_hits'][0]['track']['storeId'])
+
+# Print info about how many of the songs were successfully found on google play
+print(f"Finished searching {len(google_play_ids) + missing_data} tracks on Google Play Music")
+print(f"Found: {len(google_play_ids)}, Missing: {missing_data}")
+
+## Uncomment these if you want to create a playlist on Google Play Music!
+# playlist_name = 'Lo-Fi Dialogue'
+# playlist_id = api.create_playlist(playlist_name)
+# api.add_songs_to_playlist(playlist_id, google_play_ids)
